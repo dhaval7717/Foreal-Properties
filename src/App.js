f6006dfc538983430dff7f532375fa27dad62ee7
dhaval7717 <dhavalsuthar7717@gmail.com>
2023-11-05 16:58:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,11 +15,44 @@ function App() {
 
   const [formErrors, setFormErrors] = useState({});
   const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+  const [genderError, setGenderError] = useState(false);
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
-  };
+    
+    if (name === 'email') {
+      // Check for a valid email format
+      if (value.trim() === '') {
+        e.target.setCustomValidity('Invalid email format');
+      }
+      else if (!value.match(/^\S+@\S+\.\S+$/)) {
+        e.target.setCustomValidity('Invalid Email Format');
+      } else {
+        e.target.setCustomValidity('');
+      }
+    }
+    
+    if (name === 'password' || name === 'confirm_password') {
+      // Check if Password and Confirm Password match
+      if (name === 'confirm_password' && formData.password !== value) {
+        e.target.setCustomValidity('Please enter a valid password');
+      } else {
+        e.target.setCustomValidity('');
+      }
+    }
+
+    if (name === 'gender') {
+      // Check if gender is not selected
+      if (value !== 'Male' && value !== 'Female'){
+        e.target.setCustomValidity('Please select Male or Female');
+      }  
+      else{
+        e.target.setCustomValidity('');
+      }
+    }
+      
+    };
 
   useEffect(() => {
     if (isFormSubmitted) {
@@ -30,9 +63,26 @@ function App() {
     }
   }, [isFormSubmitted]);
 
+  
   const handleSubmit = (e) => {
     e.preventDefault();
     const errors = validateForm();
+
+    if (Object.keys(errors).length === 0 && formData.gender) {
+      // Form submission logic here
+      setIsFormSubmitted(true);
+      console.log('Form submitted with data:', formData);
+    } else {
+      // Show errors in the fields
+      setFormErrors(errors);
+  
+      if (!formData.gender) {
+        setGenderError(true);
+      } else {
+        setGenderError(false);
+      }
+    }
+
 
     if (Object.keys(errors).length === 0) {
       // Form submission logic here
@@ -51,11 +101,13 @@ function App() {
       errors.name = 'Name is required';
     }
     if (!formData.email.match(/^\S+@\S+\.\S+$/)) {
-      errors.email = 'Email is invalid';
+      errors.email = 'Invalid Email Format';
+    } else if (!formData.email) {
+      errors.email = 'Email is required';
     }
    
     if (formData.password !== formData.confirm_password) {
-      errors.confirm_password = 'Passwords do not match';
+      errors.confirm_password = 'Password and Confirm Password should be same ';
     }
 
     return errors;
@@ -85,13 +137,13 @@ function App() {
      <br/><br/>
 
   <label htmlFor="email">Email*</label>
-            <input type="email"
-             id="email" name="email"
-             required placeholder="Enter your Email"
-             onChange={handleInputChange}
-             value={formData.email}
-             />
-            {formErrors.email && <p className="error">{formErrors.email}</p>}
+     <input type="email"
+      id="email" name="email"
+      required placeholder="Enter your Email"
+      onChange={handleInputChange}
+      value={formData.email}
+       />
+    {formErrors.email && <p className="error">{formErrors.email}</p>}
              <br/><br/>
 
           
@@ -104,7 +156,7 @@ function App() {
           placeholder="Create a Password"
           onChange={handleInputChange}
           value={formData.password}/>
-          {formErrors.password && <p className="error">{formErrors.password}</p>}
+         
           <br/><br/>
 
             {/*confirm password */}
@@ -116,33 +168,37 @@ function App() {
             placeholder="Confirm Password"
             onChange={handleInputChange}
             value={formData.confirm_password}/>
-             {formErrors.confirm_password && (
-            <p className="error">{formErrors.confirm_password}</p>
-          )}
+             
+            
             <br/><br/>
             {/*Gender Section */}
-            <label for="gender">Select Gender</label>
+           
+            <label htmlFor="gender">Select Gender</label>
             <div class="gender_select">
               {/*Male */}
             <input class="male_in"
-             type="radio"
+              type="radio"
               id="male"
-              name="male"
+              name="gender"
+              value="Male"
+              checked={formData.gender === 'Male'}
               onChange={handleInputChange}
-              value={formData.male}/>
+              required/>
             <label htmlFor="male"><span class="gender_label">Male</span></label>
 
             <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
             {/*Female */}
             <input class="female_in"
-            type="radio" 
-            id="female" 
-            name="female" 
+            type="radio"
+            id="female"
+            name="gender"
+            value="Female"
+            checked={formData.gender === 'Female'}
             onChange={handleInputChange}
-            value={formData.female}/>
+            required/>
             <label htmlFor="female"><span class="gender_label">Female</span></label>
             </div>
-
+            {formErrors.gender && <p className="error">{formErrors.gender}</p>}
           {/*Birth date */}
           <br/>
             <label htmlFor="birthdate">Select Birthday Date</label>
@@ -188,12 +244,14 @@ function App() {
           </div>
          
   </form>
-    
+  {genderError && <p className="error">Please select your gender*</p>}
   {isFormSubmitted && (
       <>
        <p className="success-box">Thank you for signing up!</p>
        <img class="image" src="./timer-time.gif" />
       </>      )}
+
+    
  
 </div>
     </>
